13b396c69a8baeabcef8053974821e0e4f1cfe76
Nicholas Saloufakos <nsaloufakos@gmail.com>
2023-01-21 03:04:35 -0500

    Added Comments

--- Chess/castleRights.java
+++ Chess/castleRights.java
@@ -1,11 +1,11 @@
 package Chess;
 
-public class castleRights {
-    static public boolean wks;
-    static public boolean wqs;
-    static public boolean bks;
-    static public boolean bqs;
-    public castleRights(boolean wksInput, boolean wqsInput, boolean bksInput, boolean bqsInput){
+public class castleRights { // create a simple class that stores which of the 4 possible castling directions are still valid
+    static public boolean wks; // white king side
+    static public boolean wqs; // white queen side
+    static public boolean bks; // black king side
+    static public boolean bqs; // black queen side
+    public castleRights(boolean wksInput, boolean wqsInput, boolean bksInput, boolean bqsInput){ // castleRights constructor, is run everytime a castleRights object is created
         wks = wksInput;
         wqs = wqsInput;
         bks = bksInput;
